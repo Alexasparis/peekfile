8f99c3e0139fba7e0f94d76d54544642a15205c5
Alexasparis <alexasparis@gmail.com>
2023-10-26 16:37:50 +0200

    message

--- peek.sh
+++ peek.sh
@@ -1,14 +1,4 @@
-#!/bin/bash
-
-input_file="$1"
-lines_to_display="$2"
-
-# Display the specified number of lines from the beginning
-head -n "$lines_to_display" "$input_file"
-
-# Display "..."
+head -n $2 $1
 echo "..."
-
-# Display the specified number of lines from the end
-tail -n "$lines_to_display" "$input_file"
+tail -n "$2 $1
 
