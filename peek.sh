bb2f59473ca9c80fdf401b9e39f84a5695133924
Alexasparis <alexasparis@gmail.com>
2023-11-02 15:29:08 +0100

    message

--- peek.sh
+++ peek.sh
@@ -1,4 +1,15 @@
-head -n $2 $1
-echo "..."
-tail -n "$2 $1
+if [ -z "$2" ]; then
+    set -- "$1" 3
+fi
+
+if [ "$(wc -l<"$1")" -le "$((2*$2))" ]; then
+    echo "File contains less than $((2*$2)) lines. Full content of the "$1" is shown"
+    cat "$1"
+else
+    echo "File contains more than $((2*$2)) lines"
+    head -n "$2" "$1"
+    echo "..."
+    tail -n "$2" "$1"
+fi
+
 
