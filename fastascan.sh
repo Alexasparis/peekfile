4846536e4e1cead4539a5e1173ddec057a2840a4
Alexasparis <alexasparis@gmail.com>
2023-11-14 18:16:19 +0100

    message

--- fastascan.sh
+++ fastascan.sh
@@ -15,9 +15,29 @@ if [[ $(find $folder -type f -name "*fa" -o -name "*fasta"|wc -l) -gt 0 ]]; then
 echo "1) Number of .fa and .fasta files => $(find $folder -type f -name "*fa" -o -name "*fasta"|wc -l)"
 
 unique=$(awk '/>/{print $1}' $(find $folder -type f -name "*fasta" -o -name "*fa") | sort | uniq | wc -l)
-
 total=$(awk '/>/{print $1}' $(find $folder -type f -name "*fasta" -o -name "*fa") | wc -l)
-
 echo "2) Number of unique IDs is $unique form $total total IDs"
 
+#Report for each files
+echo "3) Report for each file"
+for i in $(find $folder -type f -name "*fa" -o -name "*fasta"); do
+echo "--> File: $(basename $i)" #Header
+
+# Check if it is a symlink or not --> Shouldnt be because we are searching for files (-o type l)
+if [[ -h $i ]]; then
+echo "IS a symlink"
+else 
+echo "NOT a symlink"
+fi
+
+# Number of sequences
+echo "There are $(grep -c ">" $i) sequences."
+
+
+
+done
+
 fi 
+
+
+
