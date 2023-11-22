acaac32c5c96a9c95b25cea3d9a723f00cf0645b
Alexasparis <alexasparis@gmail.com>
2023-11-22 10:11:55 +0100

    message

--- fastascan.sh
+++ fastascan.sh
@@ -16,8 +16,8 @@ echo "The search has been performed in $folder"
 echo
 
 #Find and count fasta and fa files and symlinks
-files=$(find $folder -type f -o -type l -name "*fa" -o -name "*fasta" ! -type d)
-n_files=$(find $folder -type f -o -type l -name "*fa" -o -name "*fasta" ! -type d | wc -l)
+files=$(find "$folder" \( -type l -o -type f \) \( -name "*.fa" -o -name "*.fasta" \))
+n_files=$(find "$folder" \( -type f -o -type l \) \( -name "*fa" -o -name "*fasta" \) | wc -l)
 
 if [[ $n_files -gt 0 ]]; then #check if fa and fastafiles present in the folder and subfolders
 
@@ -34,25 +34,37 @@ echo
 ### REPORT FOR EACH FILE
 echo "3) Report for each file"
 echo
+
 for i in $files; do
 
 #Header
-echo "-------------------------------- FILE: $(echo $i|awk -F"/" '{print $NF}')"
+echo "----- FILE: $(echo $i)" #If we want only the filename $(echo $i|awk -F"/" '{print $NF}')"
 echo       
 	
-# Check if it is a symlink or not --> Shouldnt be, we are searching for files (-o type l)
+# Check if it is a symlink or not 
 if [[ -h $i ]]; then
 echo "  3.1) IS a symlink"
-continue # skip symlinks to be analyzed
 else 
 echo "  3.1) Is NOT a symlink"
 fi
 
 #Number of sequences in the fasta files
-echo "  3.2) There are $(grep -cI ">" $i) sequences in the file."
+echo "  3.2) There are $(grep -c -I ">" $i) sequences in the file."
+if [[ $(grep -c -I ">" $i) -eq 0 ]]; then
+echo "Skipping the rest of the iteration as it does not have sequences"
+echo
+continue
+fi
 
 #Total sequence length:
-echo "  3.3) The sum of all sequences length is $(grep -vI ">" $i| grep -Iio [A-Z]|wc -l)"
+echo "  3.3) The total sequences length is $(grep -v ">" $i| grep -io [A-Z]|wc -l)"
+
+#Check if there are nt or proteic secuences
+if grep -v ">" $i | tr -d '\n' | grep -Eiq "^[ATGCU-]+$"; then 
+echo "  3.4) There are nucleotidic sequences in the file"
+else 
+echo "  3.4) There are proteic sequences in the file"
+fi
 
 #Display lines
 linecount=$(wc -l < $i)
@@ -61,29 +73,24 @@ echo "  3.4) The file has $linecount lines."
 else
 
 if [[ $linecount -le 2*$lines ]]; then
-echo "  3.4) The file has less than $((2*$lines)) lines. Displaying full content"
+echo "  3.5) The file has less than $((2*$lines)) lines. Displaying full content"
 cat $i
 else
-echo "  3.4) The file has $linecount lines. Displying first and last $lines lines."
+echo "  3.5) The file has $linecount lines. Displying first and last $lines lines."
 echo
-echo "===> First $lines lines: $(head -n $lines $i)"
+echo "=> First $lines lines: $(head -n $lines $i)"
 echo "..."
-echo "===> Last $lines lines: $(tail -n $lines $i)"   
-fi
+echo "=> Last $lines lines: $(tail -n $lines $i)"   
 fi
 
-
-#Check if there are nt or proteic secuences
-if grep -v ">" $i | tr -d '\n' | grep -Eiq "^[ATGCU-]+$"; then 
-echo "  3.5) There are nucleotidic sequences in the file"
-else 
-echo "  3.5) There are proteic sequences in the file"
 fi
+
+echo
 
 done
+
 else
 echo "There are not fasta or fa files in $folder and subfolders"
-
 fi 
 
 
